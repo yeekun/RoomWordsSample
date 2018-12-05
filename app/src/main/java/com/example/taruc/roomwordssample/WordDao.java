21379df13a1653b50ac9cdcab67801a7c1286f3d
anreh <anurehka@gmail.com>
2018-12-05 14:19:47 +0800

    Initial commit

---
+++ app/src/main/java/com/example/taruc/roomwordssample/WordDao.java
@@ -0,0 +1,23 @@
+package com.example.taruc.roomwordssample;
+
+import android.arch.lifecycle.LiveData;
+import android.arch.persistence.room.Dao;
+import android.arch.persistence.room.Insert;
+import android.arch.persistence.room.OnConflictStrategy;
+import android.arch.persistence.room.Query;
+
+import java.util.List;
+
+@Dao
+public interface WordDao {
+
+    @Insert
+            (onConflict = OnConflictStrategy.IGNORE)
+
+
+
+    @Query("SELECT * from word_table ORDER BY word ASC")
+    LiveData<List<Word>> getAllWords();
+
+
+}
